83afd068388e8e453697728fac97c3d03a2a729c
augiernicolas <92588421+augiernicolas@users.noreply.github.com>
2022-01-14 22:30:03 +0100

    .

--- Query5/__init__.py
+++ Query5/__init__.py
@@ -70,7 +70,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                 cursor.execute("SELECT SUM(tTitles.runtimeMinutes)/COUNT(*) FROM tTitles {} WHERE tTitles.tconst IN ({}) AND {}".format(filter3_1,str(result1).strip('[]'),filter3_2))
                 rows = cursor.fetchall()
                 for row in rows:
-                    dataString += f"CYPHER & SQL: Durée_moyenne des films={row[0]} avec actor={actor}, director={director} and genre={genre} \n"
+                    dataString += f"CYPHER & SQL: Durée_moyenne des films={row[0]} avec actor={actor}, director={director} \n"
 
         return func.HttpResponse(dataString)
 
