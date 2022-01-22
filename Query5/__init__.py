8c70a521cdf2ded938c5a1db293b1a7149ead02c
augiernicolas <92588421+augiernicolas@users.noreply.github.com>
2022-01-22 23:03:55 +0100

    Update __init__.py

--- Query5/__init__.py
+++ Query5/__init__.py
@@ -7,8 +7,8 @@ import pyodbc as pyodbc
 import azure.functions as func
 
 """
-Pour la requête 5, spécifiez et implémentez une API qui renvoie la durée moyenne des films
-qui correspondent aux critères genre, acteur et directeur. L'interprétation de cette énoncé vous est laissée libre.
+Neo4j & SQL : Implémentez une API qui renvoie la durée moyenne des films
+qui correspondent aux critères genre, acteur et directeur.
 """
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -37,16 +37,16 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             return func.HttpResponse("Au moins une des variables d'environnement n'a pas été initialisée.", status_code=500)
 
         errorMessage = ""
-        result1 = []
+        result_inter = []
 
         try:
             logging.info("Test de connexion avec py2neo...")
             graph = Graph(neo4j_server, auth=(neo4j_user, neo4j_password))
-            filter1 = "n1.primaryName='{}'".format(actor) if actor else "true"
-            filter2 = "n2.primaryName='{}'".format(director) if director else "true"
-            producers = graph.run("MATCH (n1:Name)-[rel:ACTED_IN]->(t:Title)<-[rel2:DIRECTED]-(n2:Name) WITH DISTINCT t.tconst AS titleIds, n1, n2 WHERE {} AND {} RETURN titleIds".format(filter1, filter2))
+            filter1 = "name1.primaryName='{}'".format(actor) if actor else "true"
+            filter2 = "name2.primaryName='{}'".format(director) if director else "true"
+            producers = graph.run("MATCH (name1:Name)-[rel:ACTED_IN]->(t:Title)<-[rel2:DIRECTED]-(name2:Name) WITH DISTINCT t.tconst AS titleIds, name1, name2 WHERE {} AND {} RETURN titleIds".format(filter1, filter2))
             for producer in producers:
-                result1.append(producer['titleIds'])
+                result_inter.append(producer['titleIds'])
         except:
             errorMessage = "Erreur de connexion a la base Neo4j"
 
@@ -56,21 +56,25 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         password = os.environ["TPBDD_PASSWORD"]
         driver= '{ODBC Driver 17 for SQL Server}'
 
-        result2 = ""
+        dataString = ""
 
         if len(server)==0 or len(database)==0 or len(username)==0 or len(password)==0:
         return func.HttpResponse("Au moins une des variables d'environnement n'a pas été initialisée.", status_code=500)
 
         try:
             logging.info("Test de connexion avec pyodbc...")
-            filter3_1 = "JOIN tGenres ON tTitles.tconst = tGenres.tconst"
-            filter3_2 = "tGenres.genre = '{}'".format(genre) if genre or "1=1"
+            filter2_1 = "tGenres.genre = '{}'".format(genre) if genre or "1=1"
             with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
                 cursor = conn.cursor()
-                cursor.execute("SELECT SUM(tTitles.runtimeMinutes)/COUNT(*) FROM tTitles {} WHERE tTitles.tconst IN ({}) AND {}".format(filter3_1,str(result1).strip('[]'),filter3_2))
+                cursor.execute("SELECT SUM(tTitles.runtimeMinutes), COUNT(*) FROM tTitles JOIN tGenres ON tTitles.tconst = tGenres.tconst WHERE tTitles.tconst IN ({}) AND {}".format(str(result_inter).strip('[]'),filter2_1))
                 rows = cursor.fetchall()
-                for row in rows:
-                    dataString += f"CYPHER & SQL: Durée_moyenne des films={row[0]} avec actor={actor}, director={director} et genre={genre}  \n"
+                (duration, count) = rows[0]
+				if float(count) == 0.:
+		            errorMessage = "Pas de films trouvés"
+			        code = 404
+		        else:
+		            result = float(duration) / float(count)
+                    dataString += f"CYPHER & SQL: Durée_moyenne des films={result} avec actor={actor}, director={director} et genre={genre}  \n"
 
         return func.HttpResponse(dataString)
 
